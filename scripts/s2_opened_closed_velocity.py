bcf62db7e55c21f16c23e1e237c4d873ce0f4c4e
Sebastian Hengst <aryx.github@gmx-topmail.de>
2022-03-22 11:46:02 +0100

    S2 open count: use new version of the script

--- scripts/s2_opened_closed_velocity.py
+++ scripts/s2_opened_closed_velocity.py
@@ -39,6 +39,38 @@ SEVERITIES = ['S2']
 
 STATUS_OPEN = ['UNCONFIRMED', 'NEW', 'ASSIGNED', 'REOPENED']
 
+def get_relevant_bug_changes(bug_data, fields, start_date, end_date):
+    bug_states = {}
+    for field in fields:
+        bug_states[field] = {
+            "old": None,
+            "new": None,
+        }
+    for historyItem in bug_data['history']:
+        for change in historyItem['changes']:
+            field = change['field_name']
+            if field in fields:
+                change_time_str = historyItem['when']
+                change_time = datetime.datetime.strptime(change_time_str, '%Y-%m-%dT%H:%M:%SZ')
+                change_time = pytz.utc.localize(change_time).date()
+                if change_time < start_date:
+                    bug_states[field]["old"] = change['added']
+                elif start_date <= change_time < end_date:
+                    if bug_states[field]["old"] is None:
+                        bug_states[field]["old"] = change['removed']
+                    bug_states[field]["new"] = change['added']
+                if change_time > end_date:
+                    if bug_states[field]["old"] is None:
+                        bug_states[field]["old"] = change['removed']
+                    if bug_states[field]["new"] is None:
+                        bug_states[field]["new"] = change['removed']
+    for field in fields:
+        if bug_states[field]["old"] is None:
+            bug_states[field]["old"] = bug_data[field]
+        if bug_states[field]["new"] is None:
+            bug_states[field]["new"] = bug_data[field]
+    return bug_states
+
 def get_severity_start_and_resolved(bug_data):
     severity_start = None
     last_resolved = None
@@ -65,58 +97,101 @@ def get_severity_start_and_resolved(bug_data):
         last_resolved = None
     return [severity_start, last_resolved]
 
-def get_bugs(start_date):
+def get_bugs(time_intervals):
 
     def bug_handler(bug_data):
-        [severity_start, last_resolved] = get_severity_start_and_resolved(bug_data)
-        if last_resolved and severity_start > last_resolved:
-            return
-        bugs_data.append([
-            bug_data['id'],
-            severity_start,
-            last_resolved,
-        ])
+        creation_time = datetime.datetime.strptime(bug_data['creation_time'], '%Y-%m-%dT%H:%M:%SZ')
+        creation_time = pytz.utc.localize(creation_time).date()
+        for time_interval in time_intervals:
+            start_date = time_interval['from']
+            end_date = time_interval['to']
+            if creation_time >= end_date:
+                continue
+            # [severity_start, last_resolved] = get_severity_start_and_resolved(bug_data)
+            bug_states = get_relevant_bug_changes(bug_data, ["product", "severity", "status"], start_date, end_date)
+            if bug_states["severity"]["new"] not in SEVERITIES:
+                continue
+            if bug_states["product"]["new"] not in PRODUCTS_TO_CHECK:
+                continue
+            if bug_states["status"]["new"] in STATUS_OPEN:
+                date_label = time_interval['label']
+                if bug_data['id'] not in bugs_by_date[date_label]:
+                    bugs_by_date[date_label].append(bug_data['id'])
+
+    start_date = time_intervals[0]['from']
+
+    bugs_by_date = {}
+    for time_interval in time_intervals:
+        date_label = time_interval['to'].isoformat()
+        bugs_by_date[time_interval['label']] = []
 
     fields = [
               'id',
+              'product',
               'status',
+              'severity',
               'creation_time',
-              'cf_last_resolved',
               'history',
              ]
 
     params = {
         'include_fields': fields,
-        'product': PRODUCTS_TO_CHECK,
         'bug_severity': SEVERITIES,
         'f1': 'keywords',
         'o1': 'allwords',
         'v1': 'regression',
-        'f1': 'OP',
-        'j1': 'OR',
         'f2': 'bug_status',
-        'v2': STATUS_OPEN,
         'o2': 'anywords',
-        'f3': 'delta_ts',
-        'o3': 'greaterthan',
-        'v3': '2020-05-01',
-        'f4': 'CP',
-        'f5': 'keywords',
-        'o5': 'allwords',
-        'v5': 'regression',
+        'v2': STATUS_OPEN,
     }
-
-    params['v3'] = start_date
-
-    bugs_data = []
 
     Bugzilla(params,
              bughandler=bug_handler,
              timeout=960).get_data().wait()
 
-    bugs_data.sort(key = lambda bug_data: bug_data[0])
+    params = {
+        'include_fields': fields,
+        'bug_severity': SEVERITIES,
+        'f1': 'keywords',
+        'o1': 'allwords',
+        'v1': 'regression',
+        'f2': 'cf_last_resolved',
+        'o2': 'changedafter',
+    }
 
-    return bugs_data
+    params['v2'] = start_date
+
+    Bugzilla(params,
+             bughandler=bug_handler,
+             timeout=960).get_data().wait()
+
+    params = {
+        'include_fields': fields,
+        'f1': 'keywords',
+        'o1': 'allwords',
+        'v1': 'regression',
+        'f2': 'OP',
+        'j2': 'AND_G',
+        'f3': 'bug_severity',
+        'o3': 'changedfrom',
+        'v3': 'S2',
+        'f4': 'bug_severity',
+        'o4': 'changedafter',
+        'f5': 'CP',
+    }
+
+    params['v4'] = start_date
+
+    Bugzilla(params,
+             bughandler=bug_handler,
+             timeout=960).get_data().wait()
+
+    open_bug_count_by_day = []
+    bugs_by_date_list = sorted([{key: value} for key, value in bugs_by_date.items()], key = lambda item: list(item.keys())[0])
+    for bugs_for_single_day_dict in bugs_by_date_list:
+        key = list(bugs_for_single_day_dict.keys())[0]
+        open_bug_count_by_day.append({key: len(bugs_for_single_day_dict[key])})
+    return open_bug_count_by_day
 
 def measure_data(time_intervals):
     for time_interval in time_intervals:
@@ -127,20 +202,19 @@ def measure_data(time_intervals):
     return data_by_time_intervals
 
 def write_csv(bug_data):
-    with open('data/s2_opened_closed_velocity.csv', 'w') as Out:
+    with open('data/s2_opened_closed_velocity_v2.csv', 'w') as Out:
         writer = csv.writer(Out, delimiter=',')
 
         writer.writerow(['Bugs with severity S2'])
         writer.writerow([])
 
-        writer.writerow([
-          'id',
-          'severity_start',
-          'last_resolved',
-        ])
+        row = ['date'] + [list(day_data.keys())[0] for day_data in bug_data]
+        writer.writerow(row)
 
-        for bug_row in bug_data:
-            writer.writerow(bug_row)
+        row = ['bug_count'] + [list(day_data.values())[0] for day_data in bug_data]
+        writer.writerow(row)
+        # for bug_row in bug_data:
+        #     writer.writerow(bug_row)
 
 parser = argparse.ArgumentParser(description='Count open, opened and closed Firefox bugs with severity S1 or S2 by developmen cycle or week')
 parser.add_argument('--start-date', type=str,
@@ -152,8 +226,24 @@ args = parser.parse_args()
 debug = args.debug
 
 # Close to date when 'S<number>' severities replaced 'major', 'minor' etc.
-start_date = args.start_date if args.start_date else '2020-05-01'
+start_date = args.start_date if args.start_date else '2022-01-02'
 
-bugs_data = get_bugs(start_date)
+start_day = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+
+time_intervals = []
+# First Sunday of the year
+from_day = start_day - datetime.timedelta(7)
+day_max = min(datetime.datetime(2022, 7, 1), datetime.datetime.now())
+while from_day < day_max:
+    to_day = from_day + datetime.timedelta(7)
+    time_intervals.append({
+        'from': from_day.date(),
+        'to': to_day.date(),
+        'label': to_day.date().isoformat(),
+    })
+    from_day += datetime.timedelta(7)
+# time_intervals.reverse()
+
+bugs_data = get_bugs(time_intervals)
 write_csv(bugs_data)
 
