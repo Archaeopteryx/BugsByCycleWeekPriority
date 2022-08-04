c0bb9505aa3e81c44e467e4c01b587f10857c4fa
Sebastian Hengst <aryx.github@gmx-topmail.de>
2022-08-04 10:38:37 +0200

    bugzilla: support value identification for fields with multiple values (e.g. keywords, groups) at a given date and time

--- scripts/utils/bugzilla.py
+++ scripts/utils/bugzilla.py
@@ -9,10 +9,12 @@ import datetime
 import json
 import pytz
 import urllib.request
+import sys
 
 BUGZILLA_CONFIG_URL = 'https://bugzilla.mozilla.org/rest/configuration'
 
 COMPONENT_TO_TEAM_MAP = None
+FIELDS_TYPES_MAP = None
 
 def get_component_to_team(product, component):
     global COMPONENT_TO_TEAM_MAP
@@ -46,6 +48,19 @@ def get_component_to_team(product, component):
         return COMPONENT_TO_TEAM_MAP[product_component_requested]
     return None
 
+
+def get_field_type(field_name, field_value):
+    global FIELDS_TYPES_MAP
+    if FIELDS_TYPES_MAP is None:
+        FIELDS_TYPES_MAP = {}
+    if field_name in FIELDS_TYPES_MAP:
+        return FIELDS_TYPES_MAP[field_name]
+    field_type = type(field_value)
+    if field_type not in [list, str]:
+        sys.exit(f"Unsupported field type '{field_type}'")
+    FIELDS_TYPES_MAP[field_name] = field_type
+
+
 def get_relevant_bug_changes(bug_data, fields, start_date, end_date):
     bug_states = {}
     for field in fields:
@@ -56,7 +71,7 @@ def get_relevant_bug_changes(bug_data, fields, start_date, end_date):
     for historyItem in bug_data['history']:
         for change in historyItem['changes']:
             field = change['field_name']
-            if field in fields:
+            if field in fields and get_field_type(field, bug_data[field]) == str:
                 change_time_str = historyItem['when']
                 change_time = datetime.datetime.strptime(change_time_str, '%Y-%m-%dT%H:%M:%SZ')
                 change_time = pytz.utc.localize(change_time).date()
@@ -71,6 +86,30 @@ def get_relevant_bug_changes(bug_data, fields, start_date, end_date):
                         bug_states[field]["old"] = change['removed']
                     if bug_states[field]["new"] is None:
                         bug_states[field]["new"] = change['removed']
+
+    for historyItem in reversed(bug_data['history']):
+        change_time_str = historyItem['when']
+        change_time = datetime.datetime.strptime(change_time_str, '%Y-%m-%dT%H:%M:%SZ')
+        change_time = pytz.utc.localize(change_time).date()
+        for change in historyItem['changes']:
+            field = change['field_name']
+            if field in fields and get_field_type(field, bug_data[field]) == list:
+                if bug_states[field]["new"] is None:
+                    # state value is an Array/List, but changes are a singe string
+                    bug_states[field]["new"] = set(bug_data[field])
+                removed_items = set(items_str_to_list(change['removed']))
+                added_items = set(items_str_to_list(change['added']))
+                if change_time > end_date:
+                    bug_states[field]["new"] = (bug_states[field]["new"] | removed_items) - added_items
+                elif start_date <= change_time < end_date:
+                    if bug_states[field]["old"] is None:
+                        bug_states[field]["old"] = bug_states[field]["new"]
+                    bug_states[field]["old"] = (bug_states[field]["old"] | removed_items) - added_items
+                if change_time < start_date:
+                    if bug_states[field]["old"] is None:
+                        bug_states[field]["old"] = bug_states[field]["new"]
+                    break
+
     for field in fields:
         if bug_states[field]["old"] is None:
             bug_states[field]["old"] = bug_data[field]
@@ -79,3 +118,7 @@ def get_relevant_bug_changes(bug_data, fields, start_date, end_date):
     return bug_states
 
 
+def items_str_to_list(items_str):
+    if not isinstance(items_str, str):
+        sys.exit(f"items provided should be string but got {type(items_str)} for {str(items_str)}")
+    return [item.strip() for item in items_str.split(",") if item.strip() != ""]
