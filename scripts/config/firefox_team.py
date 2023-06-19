c22cfa17d5ae4589a458949ab35633070e8e30de
Sebastian Hengst <aryx.github@gmx-topmail.de>
2023-06-19 15:02:18 +0200

    Firefox frontend report: add components created by split of 'Toolkit :: Notifications and Alerts' (bug 1838912)

--- scripts/config/firefox_team.py
+++ scripts/config/firefox_team.py
@@ -8,6 +8,8 @@ PRODUCTS_TO_CHECK = [
     'Toolkit',
 ]
 
+# Don't remove products and components renamed later or disabled in Bugzilla.
+# They are needed to match these combinations for past dates.
 PRODUCTS_COMPONENTS_TO_CHECK = [
     ['Core', 'Window Management'],
     ['Core', 'XUL'],
@@ -35,10 +37,13 @@ PRODUCTS_COMPONENTS_TO_CHECK = [
     ['Firefox', 'Top Sites'],
     ['Firefox', 'Tours'],
     ['Firefox', 'View'],
+    ['Toolkit', 'Content Prompts'],
     ['Toolkit', 'Downloads API'],
     ['Toolkit', 'General'],
-    ['Toolkit', 'Notifications and Alerts'],
+    ['Toolkit', 'Notifications and Alerts'], # split up in bug 1838915
     ['Toolkit', 'Picture-in-Picture'],
+    ['Toolkit', 'Popup Blocker'],
+    ['Toolkit', 'PopupNotifications and Notification Bars'],
     ['Toolkit', 'Preferences'],
     ['Toolkit', 'Printing'],
     ['Toolkit', 'Reader Mode'],
